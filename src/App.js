a1e311bd46e502eff63d6ac6bb20c09649fd60ef
jaakko koskela <jaska.koskela@gmail.com>
2020-03-12 13:34:26 +0200

    punanen

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       
-      <h1 style={{color: "white", fontSize:"80px"}}>KORONA</h1>
+      <h1 style={{color: "darkred", fontSize:"80px"}}>KORONA</h1>
       <StartPage/>
     </div>
   );
